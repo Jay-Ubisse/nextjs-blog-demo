1387c818d6577b44d03bd698373a35ecdd8f5d3c
yurannhassengo <yurannhassengo@gmail.com>
2024-10-02 09:21:01 -0700

    update layout

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,9 +1,12 @@
-
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
 
 const PagesLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col min-h-screen text-primary">
+      <Header />
       <main className="flex flex-1">{children}</main>
+      <Footer />
     </div>
   );
 };
